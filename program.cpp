e63ede30e7a27d39669aeb5e534f4c72ad13ba87
Pablofls <pablo.flsrdz@gmail.com>
2025-04-25 12:46:26 -0600

    Agregando header

---
+++ program.cpp
@@ -0,0 +1,17 @@
+/*
+#Program: Personnel Management
+#Objective: Modify personnel...
+#Author: Pablo Flores Rodriguez
+#Author: Ethan Rivera Saldivar
+#Author:Christopher Reeker Sireno
+#Author:Arturo Vargas Espinosa
+#Date: April 25th, 2025
+#Class: Arquitectura de software.
+#Honor pledge: We hereby affirm that we have done this activity with academic integrity.
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
